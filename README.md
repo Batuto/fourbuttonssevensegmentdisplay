3c2ac1cd063b05803eb2b7e668549a02d65ca823
batuto <mariano@vauxoo.com>
2017-09-12 00:31:17 -0500

    [ADD] Initial commit, adding initial files

---
+++ README.md
@@ -0,0 +1,6 @@
+# This is for educational and demonstrative purposes
+
+This program is designed to take an output from 0 to F in a 7-segment display with an input from four buttons.
+
+## You can use it as you prefer
+
---
+++ sevensegmentfourbutton.ino
@@ -0,0 +1,57 @@
+// Author: Batuto
+// GitHub: github.com/batuto
+// This code was designed using an Arduino Uno (and Nano) as reference, but may work in other devices,
+// take this in mind when porting to another device.
+
+// This program takes a four bit input and write its in a seven segment display
+
+
+int i, value;
+int list[][7] = {{0,1,2,3,4,5,5},  // 0
+                 {2,3,3,3,3,3,3},  // 1
+                 {0,1,3,4,6,6,6},  // 2
+                 {1,2,3,4,6,6,6},  // 3
+                 {2,3,5,6,6,6,6},  // 4
+                 {1,2,4,5,6,6,6},  // 5
+                 {0,1,2,4,5,6,6},  // 6
+                 {2,3,4,4,4,4,4},  // 7
+                 {0,1,2,3,4,5,6},  // 8
+                 {2,3,4,5,6,6,6},  // 9
+                 {0,2,3,4,5,6,6},  // A
+                 {0,1,2,5,6,6,6},  // B
+                 {0,1,4,5,5,5,5},  // C
+                 {0,1,2,3,6,6,6},  // D
+                 {0,1,4,5,6,6,6},  // E
+                 {0,4,5,6,6,6,6}}; // F
+/*
+The previous matrix is defined to use a
+ seven display numerating each segment
+ in the next manner:
+     4
+ ========
+||      ||
+||5     ||3
+||      ||
+6========
+||      ||
+||0     ||2
+||      ||
+ ========
+    1
+*/
+
+void setup(){
+  for(i = 0; i < 8; i++)
+    pinMode(i, OUTPUT);
+  for(i = 8; i < 12; i++)
+    pinMode(i, INPUT);  
+}
+
+void loop(){
+  PORTD = 0;
+  value = PINB;
+  for(i = 0; i < 7; i++)
+    digitalWrite(list[value][i], HIGH);
+   delay(800);
+}
+
